3f8432afaa092e0f683f03e1e64a786ee910114e
Morris, Michael <mmorris1990@gmail.com>
2019-05-25 10:26:31 -0500

    assigned global variables

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+// VARIABLES //
+
+var questionCounter = 0;
+
+var answerTimeout = 3000;
+
+var corrent = 0;
+
+var incorrect = 0;
+
+var userAnswer = [];
+
+var questions = [
+    {
+        question: "The National Hockey League only consists of teams within the USA:",
+        choices: ["True" , "False"],
+        choicesAnswer: 1
+    },
+    {
+        question: "When a player scores three goals in a single game it is called a:",
+        choices: ["Three's Company" , "Grand Slam" , "Hat Trick" , "Triple Crown"],
+        choicesAnswer: 2
+    },
+    {
+        question: "The NHL Championship trophy is named after:",
+        choices: ["Sir Dudley" , "Earl Aurthur" , "Duke Ferdinand" , "Lord Stanley"],
+        choicesAnswer: 3
+    },
+    {
+        question: "How many periods of play are there in a regulation game?",
+        choices: [1 , 2 , 3 , 4],
+        choicesAnswer: 2
+    }];
+
+// FUNCTIONS //
+
+
+})
