084460fa52551f22b1c1809cafb2fca52219420d
Morris, Michael <mmorris1990@gmail.com>
2019-05-30 14:33:00 -0500

    fixed review screen issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     var correct = 0;
 
     var incorrect = 0;
- 
+
     var userAnswer = [];
 
     var questions = [
@@ -36,6 +36,41 @@ $(document).ready(function () {
 
     // FUNCTIONS //
 
+    // Display start page
+    function displayStart() {
+        $("#content").append("<button class='btn btn-light btn-lg' id='start-button'>" + "Faceoff" + "</button>");
+        // Button starts the game
+        $("#start-button").on("click", function (start) {
+            start.preventDefault();
+            firstQuestion();
+            resetTimer();
+            runTimer();
+        });
+    };
+
+    // Display the first question page
+    function firstQuestion() {
+        var start = $("#content");
+        start.empty();
+        displayQuestion();
+    };
+
+    // Display the current question page
+    function displayQuestion() {
+        // clear previous quiz form and timer
+        clearQuestion();
+        stopTimer();
+        resetTimer();
+        // display question
+        $(".questionX").html(questions[questionCounter].question);
+        // call quiz form function
+        createRadios();
+        // create submit button
+        $("#submit-div").append("<button type='submit' class='btn btn-light' id='submit'>" + "Shoot" + '</button');
+        runTimer();
+        submitAnswer();
+    };
+
     // Submit user answers
     function submitAnswer() {
         $("#submit").on("click", function (submit) {
@@ -52,7 +87,7 @@ $(document).ready(function () {
     };
 
     // Timer functions
-    var timeLeft = 10;
+    var timeLeft = 12;
     var increment;
 
     function runTimer() {
@@ -89,88 +124,22 @@ $(document).ready(function () {
         clearTimeout(increment);
     };
 
-    // Display quiz form
-    function createRadios() {
-        var responseOptions = $("#responses");
-        // empty array from previous question
-        responseOptions.empty();
+    // Change to the next question or end the quiz
+    function nextQuestion() {
+        // check for correct answer
+        checkQuestion();
 
-        // create multiple choice input
-        for (var i = 0; i < questions[questionCounter].choices.length; i++) {
-            responseOptions.append("<input type='radio' name='optionRadios' value='" + [i] + "'><div class='options'>" + questions[questionCounter].choices[i] + "</div></input>");
+        questionCounter++;
+        // either end the quiz or display next question
+        if (questionCounter === questions.length) {
+            setTimeout(displayEnd, answerTimeout);
+        }
+
+        else {
+            setTimeout(displayQuestion, answerTimeout);
         };
     };
 
-    // Display the current question page
-    function displayQuestion() {
-        // clear previous quiz form and timer
-        clearQuestion();
-        stopTimer();
-        resetTimer();
-        // display question
-        $(".questionX").html(questions[questionCounter].question);
-        // call quiz form function
-        createRadios();
-        // create submit button
-        $("#submit-div").append("<button type='submit' class='btn btn-light' id='submit'>" + "Shoot" + '</button');
-        runTimer();
-        submitAnswer();
-    };
-
-    // Display start page
-    function displayStart() {
-        $("#content").append("<button class='btn btn-light btn-lg' id='start-button'>" + "Faceoff" + "</button>");
-        // Button starts the game
-        $("#start-button").on("click", function (start) {
-            start.preventDefault();
-            firstQuestion();
-            resetTimer();
-            runTimer();
-        });
-    };
-
-    // Reset variables for end of game
-    function reset() {
-        questionCounter = 0;
-        correct = 0;
-        incorrect = 0;
-        userAnswer = [];
-        resetTimer();
-    };
-
-    // Display end of game
-    function displayEnd() {
-        clearQuestion();
-        // shows the final score
-        $("#content").append("<h2>" + "Correct answers: " + correct + "</h2> <br> <h2>" + "Incorrect answers: " + incorrect + "</h2> <br> <h2> <button class='btn btn-primary btn-lg' id='restart-button'>" + "Rematch" + "</button");
-
-        // restarts game
-        $("#restart-button").on("click", function (restart) {
-            restart.preventDefault();
-            // show first question
-            reset();
-            clearQuestion();
-            displayStart();
-        });
-    };
-
-    // Clear quiz to prep for next part of game
-    function clearQuestion() {
-        var questionDiv = $(".questionX");
-        questionDiv.empty();
-
-        var responseDiv = $("#responses");
-        responseDiv.empty();
-
-        var submitDiv = $("#submit-div");
-        submitDiv.empty();
-
-        var contentDiv = $("#content");
-        contentDiv.empty();
-
-        stopTimer();
-    };
-    
     // Check user choice with the correct answer for each question
     function checkQuestion() {
         // clear current quiz form
@@ -201,27 +170,58 @@ $(document).ready(function () {
         };
     };
 
-    // Change to the next question or end the quiz
-    function nextQuestion() {
-        // check for correct answer
-        checkQuestion();
+    // Clear quiz to prep for next part of game
+    function clearQuestion() {
+        var questionDiv = $(".questionX");
+        questionDiv.empty();
 
-        questionCounter++;
-        // either end the quiz or display next question
-        if (questionCounter === questions.length) {
-            setTimeout(displayEnd, answerTimeout);
-        }
+        var responseDiv = $("#responses");
+        responseDiv.empty();
 
-        else {
-            setTimeout(displayQuestion, answerTimeout);
+        var submitDiv = $("#submit-div");
+        submitDiv.empty();
+
+        var contentDiv = $("#content");
+        contentDiv.empty();
+
+        stopTimer();
+    };
+
+    // Display quiz form
+    function createRadios() {
+        var responseOptions = $("#responses");
+        // empty array from previous question
+        responseOptions.empty();
+
+        // create multiple choice input
+        for (var i = 0; i < questions[questionCounter].choices.length; i++) {
+            responseOptions.append("<input type='radio' name='optionRadios' value='" + [i] + "'><div class='options'>" + questions[questionCounter].choices[i] + "</div></input>");
         };
     };
 
-    // Display the first question page
-    function firstQuestion() {
-        var start = $("#content");
-        start.empty();
-        displayQuestion();
+    // Reset variables for end of game
+    function reset() {
+        questionCounter = 0;
+        correct = 0;
+        incorrect = 0;
+        userAnswer = [];
+        resetTimer();
+    };
+
+    // Display end of game
+    function displayEnd() {
+        clearQuestion();
+        // shows the final score
+        $("#content").append("<h2>" + "Correct answers: " + correct + "</h2> <br> <h2>" + "Incorrect answers: " + incorrect + "</h2> <br> <h2> <button class='btn btn-primary btn-lg' id='restart-button'>" + "Rematch" + "</button");
+
+        // restarts game
+        $("#restart-button").on("click", function (restart) {
+            restart.preventDefault();
+            // show first question
+            reset();
+            clearQuestion();
+            displayStart();
+        });
     };
 
     // Starts game on page load
